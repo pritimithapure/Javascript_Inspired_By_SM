849ed222627581265089f5cdd37e59d9287d9122
pritimithapure <156600913+pritimithapure@users.noreply.github.com>
2026-02-08 14:19:46 +0530

    Add files via upload

---
+++ Basics/1_Variables.js
@@ -0,0 +1,11 @@
+const accid=123
+
+let email="priti@gmail.com"
+var name="Priti"
+
+address ="Satara"
+
+// accid=456     // we cant change const value again 
+
+// console.log(accid)
+console.table([email,name,address])
