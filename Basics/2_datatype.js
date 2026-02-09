0d966091f0f0717e93dde4b01243b8bb77a6fe96
pritimithapure <156600913+pritimithapure@users.noreply.github.com>
2026-02-09 18:07:40 +0530

    Add files via upload

---
+++ Basics/2_datatype.js
@@ -0,0 +1,11 @@
+"use strict" // it will treat  js code   as newest version
+
+let logged=true;
+
+console.log(typeof "priiti")
+console.log(typeof logged)
+
+console.log(typeof null)// object
+
+console.log(typeof undefined)  // undefined 
+console.log()
---
+++ Basics/3_conversion.js
@@ -0,0 +1,48 @@
+// let score ="33";
+
+// console.log(typeof score)
+
+//  score=Number(score);
+
+// console.log(typeof score)
+
+let aphanum ="33aa";
+
+console.log(typeof aphanum)
+
+ newnum=Number(aphanum);
+
+console.log(typeof newnum)// number
+console.log(newnum)// Nan
+
+// "33"= 33
+// "33sdkj" = Nan (but type is number)
+// true= 1  false =0
+
+
+let loggedin="1";
+
+let booleanlog=Boolean(loggedin)
+console.log(typeof booleanlog)
+console.log(booleanlog) // true
+
+loggedin=""
+booleanlog=Boolean(loggedin)
+console.log(typeof booleanlog)
+console.log(booleanlog)// false
+
+
+loggedin="priti"
+booleanlog=Boolean(loggedin)
+console.log(typeof booleanlog)
+console.log(booleanlog)// true
+
+
+// 1=> true  0=>false
+// "" => false
+// "priti" => true 
+
+let num2=33; 
+let num2str=String(num2)
+console.log(num2str)
+console.log(typeof num2str)// string
